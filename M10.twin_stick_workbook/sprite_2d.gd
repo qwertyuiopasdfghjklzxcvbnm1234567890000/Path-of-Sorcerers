bdd377ba30c0538ae6525b706e32d8a153193543
qwertyuiopasdfghjklzxcvbnm1234567890000 <159198097+qwertyuiopasdfghjklzxcvbnm1234567890000@users.noreply.github.com>
2025-01-15 11:36:39 -0800

    L4 some progress

---
+++ M10.twin_stick_workbook/sprite_2d.gd
@@ -0,0 +1,15 @@
+extends Sprite2D
+const MOVE_UP := preload("res://player/godot_up.png")
+const MOVE_DOWN := preload("res://player/godot_bottom.png")
+const MOVE_RIGHT := preload("res://player/godot_right.png")
+const MOVE_UP_RIGHT := preload("res://player/godot_up_right.png")
+const MOVE_BOTTOM_RIGHT := preload("res://player/godot_bottom_right.png")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
