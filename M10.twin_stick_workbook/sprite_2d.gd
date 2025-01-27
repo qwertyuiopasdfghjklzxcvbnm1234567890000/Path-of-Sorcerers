882405b0a71d90b1dca8651139a316bfa6bb8862
qwertyuiopasdfghjklzxcvbnm1234567890000 <159198097+qwertyuiopasdfghjklzxcvbnm1234567890000@users.noreply.github.com>
2025-01-27 11:11:55 -0800

    L5 started

--- M10.twin_stick_workbook/sprite_2d.gd
+++ M10.twin_stick_workbook/sprite_2d.gd
@@ -5,11 +5,25 @@ const MOVE_RIGHT := preload("res://player/godot_right.png")
 const MOVE_UP_RIGHT := preload("res://player/godot_up_right.png")
 const MOVE_BOTTOM_RIGHT := preload("res://player/godot_bottom_right.png")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
+const UP_RIGHT = Vector2.UP + Vector2.RIGHT
+const UP_LEFT = Vector2.UP + Vector2.LEFT
+const DOWN_RIGHT = Vector2.DOWN + Vector2.RIGHT
+const DOWN_LEFT = Vector2.DOWN + Vector2.LEFT
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var input_direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+
+	var direction_discrete := input_direction.sign()
+	match direction_discrete:
+		Vector2.RIGHT, Vector2.LEFT:
+			texture = MOVE_BOTTOM_RIGHT
+		Vector2.UP:
+			texture = MOVE_UP
+		Vector2.DOWN:
+			texture = MOVE_DOWN
+		UP_RIGHT, UP_LEFT:
+			texture = MOVE_UP_RIGHT
+		DOWN_RIGHT, DOWN_LEFT:
+			texture = MOVE_BOTTOM_RIGHT
+	if direction_discrete.length() > 0:
+		flip_h = direction_discrete.x < 0.0
